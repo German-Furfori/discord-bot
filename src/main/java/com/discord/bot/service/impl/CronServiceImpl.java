2a95cef88123e8e77c1d1918e26e0e1ae20718d1
Germán Furfori <german.furfori@gmail.com>
2024-08-17 17:02:15 -0300

    fix service implementation

--- src/main/java/com/discord/bot/service/impl/CronServiceImpl.java
+++ src/main/java/com/discord/bot/service/impl/CronServiceImpl.java
@@ -1,5 +1,6 @@
 package com.discord.bot.service.impl;
 
+import com.discord.bot.service.CronService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -7,6 +8,6 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class CronServiceImpl {
+public class CronServiceImpl implements CronService {
 
 }
